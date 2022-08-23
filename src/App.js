6e25a7dcb7e585db560523ad13d21456c8e4341f
Amaan Khan <Amaan.khan@connectwise.com>
2022-08-23 22:30:16 +0530

    feat: grammify frontend added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import axios from 'axios';
+export default function App() {
 
-function App() {
+  const [text, setText] = useState("");
+  const [flag, setFlag] = useState(false);
+  const [errors, setErrors] = useState([]);
+  const verify = () => {
+    setFlag(true);
+    console.log("Verifying....");
+    const dict = {
+      "text": text
+    }
+    axios.post("http://localhost:8000/", dict).then((res) => {
+      setText(res.data.text);
+      setErrors(res.data.errors);
+      setFlag(false);
+    });
+  };
+
+  const handleChange = (e) => {
+    setText(e.target.value);
+    // setCount(e.target.value.length)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <>
+      <div style={{ marginTop: -60 }} className="container">
+        <h2>Grammify!</h2>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Check your English text for grammar, spelling, and punctuation errors with
+          Grammify!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <textarea
+          id="textarea"
+          className="textarea"
+          placeholder="Write here..."
+          maxLength={150}
+          defaultValue={text}
+          value={text}
+          onChange={(e) => handleChange(e)}
+        />
+        {!flag ? (<button onClick={verify} >Verify</button>) : (<button disabled>Verifying...</button>)}
+      </div>
+      <div style={{marginTop: -60}} class="container">
+        <ol>
+          <span style={{ fontSize: 22 }}><b>Changes</b></span>
+          <hr/>
+          {errors.map(err => (
+            <li>
+              <span style={{ color: "red" }}>{err[0]}</span> : <span style={{ color: "green" }}>{err[1]}</span>
+            </li>
+          ))}
+        </ol>
+      </div >
+    </>
+  )
 }
-
-export default App;
